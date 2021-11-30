3562f3130b7e2aedbe1ffffd9208094cc4d4abd0
Abu7864 <abu7864@gmail.com>
2021-12-01 03:07:12 +0530

    db2

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "ec2instance" {
   ami           = "ami-0a23ccb2cdd9286bb"
   instance_type = "t3.micro"
   tags = {
-    Name = "DBINSTANCE1"
+    Name = "DBINSTANCE2"
   }
 }
