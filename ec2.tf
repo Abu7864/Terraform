2ddbfc732472eae94aed280eca698e440e85aac7
Abu7864 <50233830+Abu7864@users.noreply.github.com>
2021-12-02 02:46:26 +0530

    Add files via upload

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2instance" {
   ami           = "ami-0a23ccb2cdd9286bb"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   tags = {
-    Name = "DBINSTANCE2"
+    Name = "DBINSTANCE1"
   }
 }
